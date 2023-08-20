833def7ddb44c898db0be1ff77934be2188458c3
Rodrigo Meireles de Oliveira <rodrigomeirelesdeoliveira@gmail.com>
2023-08-20 10:08:41 -0300

    Compose pegando API-KEY no .env da raiz do projeto

--- backend/server.py
+++ backend/server.py
@@ -10,21 +10,19 @@ def read_root():
 
 
 @app.post("/chat")
-async def generate_response(message: str):
+async def generate_response(data: dict):
+    message = data.get("message")
     if not message:
         raise HTTPException(status_code=400, detail="Message cannot be empty")
 
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=message,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}],
         max_tokens=50,  # You can adjust this as needed
     )
 
     if response.choices:
-        generated_text = response.choices[0].text
+        generated_text = response.choices[0].message
         return {"response": generated_text}
     else:
         raise HTTPException(status_code=500, detail="Failed to generate response")
-
-
-#
